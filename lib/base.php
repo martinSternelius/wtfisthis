d83513737a14fb4fd4baf85b0668f778f5f85d97
Karl Åström <karl.astrom@gmail.com>
2011-02-14 13:20:52 +0100

    Add comments and include-path to base.php

--- lib/base.php
+++ lib/base.php
@@ -1,6 +1,17 @@
-<?php 
+<?php
+/**
+ * include local configurations
+ */
 require_once 'config.php';
 
+/**
+ * Add lib/ to include-path so we get slightly cleaner includes
+ */
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ );
+
+/**
+ * Set autoloader for classes
+ */
 function __autoload($class_name) {
        include 'lib/classes/' . $class_name . '.class.php';
 }
