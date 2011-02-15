15cdc25a6e9f097aa5bddda2275ba7b17874e244
MSI <martin@ubuntu.(none)>
2011-02-15 10:22:48 +0100

    Test for question's save function

---
+++ tests/question.test.php
@@ -0,0 +1,15 @@
+<?php
+	require_once '../lib/base.php';
+	require_once "../lib/simpletest/unit_tester.php";
+	require_once "../lib/simpletest/autorun.php";
+	require_once '../lib/classes/Question.class.php';
+
+	class test_phpflickr extends UnitTestCase {
+				
+		public function test_save() {
+			$question = new Question($id = null, 'Test Question', 'This is a test question', 999);
+			$this->assertTrue(is_numeric($question->save()));			
+		}
+	}
+	
+?>
