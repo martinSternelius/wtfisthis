882cdf5cf82d79957698073a7324b4881d923888
Martin Sternelius <martin.sternelis@gmail.com>
2011-02-17 09:20:18 +0100

    removed unused test

--- tests/answer.test.php
+++ tests/answer.test.php
@@ -2,15 +2,10 @@
 	require_once '../lib/base.php';
 	require_once "../lib/simpletest/unit_tester.php";
 	require_once "../lib/simpletest/autorun.php";
-	require_once "../lib/classes/Answer.class";
+	require_once "../lib/classes/Answer.class.php";
 
 	class test_answer extends UnitTestCase {
 				
-		public function test_loadAnswer() {
-			$id = 1;
-			$answer = new Answer($id);
-			$this->assertEqual($id, $this->getId());
-		}
 	}
 	
 	
